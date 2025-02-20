81d164c80aeb1d269e11258e784e456770ce7bf2
tunguyenarea <tunguyenarea@gmail.com>
2025-02-20 15:55:32 +0700

    Commit 3

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,22 @@
-import SectionOne from '@/app/ui/home/section-one';
+import { Note } from '@/components/custom/note';
+import { fetchNote } from '@/app/lib/data';
 
-export default function Page() {
+export default async function Page() {
+  const listNote = await fetchNote("dd39e1da-d0d5-4797-ae22-f2f2d6010b6d");
+
   return (
   <>
 
   <main>
-    <SectionOne />
+  {listNote.map((note) => {
+      return(
+        <Note key={note.note_id}
+          noteLink={`/home/${note.note_id}`}
+          noteTitle={note.title}
+          noteContent={note.content}
+        />
+      );
+    })}
   </main>
 
   </>
